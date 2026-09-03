44534ded8769c790ce33932d1f454f551f63c361
Sharaal2703 <jospeenranisharaal@gmail.com>
2026-09-03 23:54:37 +0530

    Add: climbing-stairs - submission-0

---
+++ Data Structures & Algorithms/climbing-stairs/submission-0.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int dpClimbing(int n,int dp[])
+    {
+        if(n==0 || n==1)
+        {
+            return 1;
+        }
+        if(dp[n]!=-1)
+        {
+            return dp[n];
+        }
+        return dp[n]=dpClimbing(n-1,dp)+dpClimbing(n-2,dp);
+
+
+    }
+    public int climbStairs(int n) {
+
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        return dpClimbing(n,dp);
+        
+    }
+}
